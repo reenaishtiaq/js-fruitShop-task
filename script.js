e72b36549a8b48e9ad396911eb659ed7b0b3441a
reenaishtiaq <49521474+reenaishtiaq@users.noreply.github.com>
2019-06-20 15:25:20 +0500

    js WIP

--- script.js
+++ script.js
@@ -1,15 +1,54 @@
 
 var num = 0;
+
 function plusNumber() {
-    num = num + 1
-        document.querySelector('#fruit-rates input').value = num;
+    num = num + 1;
+        document.querySelector('#apple-rate input').value = num;
 
 }
 
 function minusNumber() {
-    num = num <= 1
-        document.querySelector('#fruit-rates input').value = num;
+    if (num <= 0) {
+        alert ('There is an Error');
+    }
+    else{
+    num = num - 1;
+        document.querySelector('#apple-rate input').value = num;
+    }
+}
+
+
+function addNumber() {
+    num = num + 1;
+        document.querySelector('#orange-rate input').value = num;
 
 }
-   
+
+function substNumber() {
+    if (num <= 0) {
+        alert ('There is an Error');
+    }
+    else{
+    num = num - 1;
+        document.querySelector('#orange-rate input').value = num;
+    }
+}
+
+
+function plsNumber() {
+    num = num + 1;
+        document.querySelector('#banana-rate input').value = num;
+
+}
+
+function mnsNumber() {
+    if (num <= 0) {
+        alert ('There is an Error');
+    }
+    else{
+    num = num - 1;
+        document.querySelector('#banana-rate input').value = num;
+    }
+}
+
 
