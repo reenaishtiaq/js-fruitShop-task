905c4022dbb95b789763d9caeae9c6397e2d1238
reenaishtiaq <49521474+reenaishtiaq@users.noreply.github.com>
2019-07-05 06:15:59 -0700

    js WIP

--- script.js
+++ script.js
@@ -1,54 +1,99 @@
 
-var num = 0;
+var myAccount = 0;
+var rupes = 0;
+var totalBill = 0;
+var appleNumber = 0;
+var orangeNumber = 0;
+var bananaNumber = 0;
 
 function plusNumber() {
-    num = num + 1;
-        document.querySelector('#apple-rate input').value = num;
+    // 1. Get the value of input which have id "account"
+    // 2. Get the value of input which have id "bill"
+    // 3. Get the value of input which have id "apple"
 
+
+    document.querySelector("#account").value;
+    document.querySelector("#bill").value = totalBill;
+    document.querySelector('#apple').value = appleNumber;
+
+
+    if (myAccount > 10) {
+        alert("You don't have sufficient money");
+    } else {
+        appleNumber += 1;
+        rupes += 10;
+        myAccount = myAccount - rupes;
+        totalBill = 10 * appleNumber;
+    }
 }
 
 function minusNumber() {
-    if (num <= 0) {
-        alert ('There is an Error');
-    }
-    else{
-    num = num - 1;
-        document.querySelector('#apple-rate input').value = num;
+    document.querySelector("#bill").value = totalBill;
+    document.querySelector('#apple').value = appleNumber;
+    if (totalBill > 0) {
+        appleNumber = appleNumber - 1;
+        rupes -= 10;
+        myAccount = myAccount + rupes;
+        totalBill = +rupes;
     }
 }
 
 
 function addNumber() {
-    num = num + 1;
-        document.querySelector('#orange-rate input').value = num;
+    document.querySelector("#account").value;
+    document.querySelector("#bill").value = totalBill;
+    document.querySelector('#orange').value = orangeNumber;
 
+    if (myAccount > 10) {
+        alert("You don't have sufficient money");
+    } else {
+        orangeNumber += 1;
+        rupes += 15;
+        myAccount = myAccount - rupes;
+        totalBill = 15 * orangeNumber;
+    }
 }
 
 function substNumber() {
-    if (num <= 0) {
-        alert ('There is an Error');
-    }
-    else{
-    num = num - 1;
-        document.querySelector('#orange-rate input').value = num;
+    document.querySelector("#bill").value = totalBill;
+    document.querySelector('#orange').value = orangeNumber;
+    if (totalBill > 0) {
+        orangeNumber = orangeNumber - 1;
+        rupes -= 15;
+        myAccount = myAccount + rupes;
+        totalBill = +rupes;
     }
 }
 
-
 function plsNumber() {
-    num = num + 1;
-        document.querySelector('#banana-rate input').value = num;
+    document.querySelector("#account").value;
+    document.querySelector("#bill").value = totalBill;
+    document.querySelector('#banana').value = bananaNumber;
 
+    if (myAccount > 10) {
+        alert("You don't have sufficient money");
+    } else {
+        bananaNumber += 1;
+        rupes += 7;
+        myAccount = myAccount - rupes;
+        totalBill = 7 * bananaNumber;
+    }
 }
 
 function mnsNumber() {
-    if (num <= 0) {
-        alert ('There is an Error');
-    }
-    else{
-    num = num - 1;
-        document.querySelector('#banana-rate input').value = num;
+    document.querySelector("#bill").value = totalBill;
+    document.querySelector('#banana').value = bananaNumber;
+
+    if (totalBill > 0) {
+        bananaNumber = bananaNumber - 1;
+        rupes -= 7;
+        myAccount = myAccount + rupes;
+        totalBill = +rupes;
     }
 }
+
+
+
+
 
 
